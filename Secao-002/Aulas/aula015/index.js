d983c46e69a522febcb29105dac3245b1d3b30d0
ThaisAlves7 <75734682+ThaisAlves7@users.noreply.github.com>
2022-10-15 20:45:26 -0300

    Manipulacao com document
    Manipulação do HTML usando getDocumentById

---
+++ Secao-002/Aulas/aula015/index.js
@@ -0,0 +1,33 @@
+//Biblioteca Math
+
+let num1 = 9;
+
+//Arrendondamento para Baixo
+// let num2 = Math.floor(num1);
+
+//Arrendondamento para Cima
+// let num2 = Math.ceil(num1);
+
+//Arrendonda o número levando a regra "para baixo < 5 ou para cima > 5"
+// let num1 = 9.34;
+// let num1 = 9.54;
+// let num2 = Math.round(num1);
+// console.log(num2);
+
+//Obtém o maior numero da sequencia
+// console.log(Math.max(1, 2, 3, 4, 5, -10, -50, 1500, 9, 8, 7, 6));
+
+//Obtém o menor numero da sequencia
+// console.log(Math.min(1, 2, 3, 4, 5, -10, -50, 1500, 9, 8, 7, 6));
+
+//Gerar números aleatórios
+// console.log(Math.random());
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+// console.log(aleatorio);
+
+//Exponencial
+// console.log(Math.pow(3, 10));
+
+//Obter raiz quadrada
+// console.log(Math.sqrt(64));
+// console.log(num1 ** 0.5)
