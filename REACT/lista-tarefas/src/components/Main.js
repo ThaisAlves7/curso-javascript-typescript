debbbef1592470de716edeeed1aefbec69bd47af
Thais Alves <thacruzz007@gmail.com>
2022-11-02 18:21:03 -0300

    Lista de Tarefas - Finalizada
    Finalização da Lista de Tarefas

--- REACT/lista-tarefas/src/components/Main.js
+++ REACT/lista-tarefas/src/components/Main.js
@@ -1,33 +1,107 @@
-import React, { Component } from "react";
-import "./Main.css";
+/* eslint-disable react/prop-types */
 
-//Form
-import { FaPlus } from "react-icons/fa";
+import React, { Component } from "react";
+import Form from "./Form";
+import Tarefas from "./Tarefas";
+
+import "./Main.css";
 
 export default class Main extends Component {
   state = {
     novaTarefa: "",
+    tarefas: [],
+    index: -1,
   };
 
+  //Carregar a lista salva no Local Storage na aplicação
+  componentDidMount() {
+    const tarefas = JSON.parse(localStorage.getItem("tarefas"));
+
+    if (!tarefas) return;
+
+    this.setState({ tarefas });
+  }
+
+  //Salvar informações no Local Storage
+  componentDidUpdate(prevProps, prevState) {
+    const { tarefas } = this.state;
+
+    if (tarefas === prevState.tarefas) return;
+
+    localStorage.setItem("tarefas", JSON.stringify(tarefas));
+  }
+
+  //Atribuir o valor do Input da nova tarefa na lista de tarefas na lista
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { tarefas, index } = this.state;
+    let { novaTarefa } = this.state;
+    novaTarefa = novaTarefa.trim();
+
+    if (tarefas.indexOf(novaTarefa) !== -1) return;
+
+    const novasTarefas = [...tarefas];
+
+    if (index === -1) {
+      this.setState({
+        tarefas: [...novasTarefas, novaTarefa],
+        novaTarefa: "",
+      });
+    } else {
+      novasTarefas[index] = novaTarefa;
+
+      this.setState({
+        tarefas: [...novasTarefas],
+      });
+    }
+  };
+
+  //Obter o valor da caixa de texto da nova tarefa
   handleChange = (e) => {
     this.setState({
       novaTarefa: e.target.value,
     });
   };
 
+  //Editar as tarefas da lista
+  handleEdit(e, index) {
+    const { tarefas } = this.state;
+
+    this.setState({
+      index,
+      novaTarefa: tarefas[index],
+    });
+  }
+
+  //Deletar as tarefas da lista
+  handleDelete(e, index) {
+    const { tarefas } = this.state;
+    const novasTarefas = [...tarefas];
+    novasTarefas.splice(index, 1);
+
+    this.setState({
+      tarefas: [...novasTarefas],
+    });
+  }
+
   render() {
-    const { novaTarefa } = this.state;
+    const { novaTarefa, tarefas } = this.state;
 
     return (
       <div className="main">
         <h1>Lista de tarefas</h1>
 
-        <form action="#" className="form">
-          <input onChange={this.handleChange} type="text" value={novaTarefa} />
-          <button type="submit">
-            <FaPlus />
-          </button>
-        </form>
+        <Form
+          handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+          novaTarefa={novaTarefa}
+        />
+
+        <Tarefas
+          tarefas={tarefas}
+          handleEdit={this.handleEdit.bind(this)}
+          handleDelete={this.handleDelete.bind(this)}
+        />
       </div>
     );
   }
