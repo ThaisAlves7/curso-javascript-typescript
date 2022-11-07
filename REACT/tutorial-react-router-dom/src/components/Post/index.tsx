a0c788b9cdee4fb6e1550eadceacdbee2180e163
Thais Alves <thacruzz007@gmail.com>
2022-11-06 23:38:15 -0300

    React Router Dom v6
    Explicação e modelos após alterações do componentes mais usados

---
+++ REACT/tutorial-react-router-dom/src/components/Post/index.tsx
@@ -0,0 +1,10 @@
+import './style.css'
+
+export const Post = () => {
+  return (
+  <div>
+    <h1>Post</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro blanditiis aperiam maiores ipsa! Dignissimos nisi ipsa, aperiam assumenda eaque non. Voluptates aspernatur temporibus recusandae harum fugiat accusamus fugit aperiam illo!</p>
+  </div>
+  )
+}
---
+++ REACT/tutorial-react-router-dom/src/main.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import './styles/global.css'
+
+import { About } from './components/About'
+import { Home } from './components/Home'
+import { Menu } from './components/Menu'
+import { NotFound } from './components/NotFound'
+import { Posts } from './components/Posts'
+import { Redirect } from './components/Redirect'
+import { Post } from './components/Post'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <Menu />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* <Route path="/posts/:id" element={<Posts />} /> */}
+        <Route path="/posts/" element={<Posts />} >
+          <Route path=':id' element={<Post/>} />
+        </Route>
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/redirect" element={<Redirect />} />
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+)
