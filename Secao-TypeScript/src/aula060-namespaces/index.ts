18c4e2b08fc0af0f6ad72594f00b65d1cb99e552
Thais Alves <thacruzz007@gmail.com>
2022-11-18 21:03:53 -0300

    TypeScript - Namespace e modules

---
+++ Secao-TypeScript/src/aula060-namespaces/index.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/triple-slash-reference */
+/*
+  /// <reference path="module.ts" />
+*/
+/// <reference path="modulo/module.ts" />
+
+console.log(MeuNamespace.nomeDoNamespace);
+console.log(constDoNamespace);
---
+++ Secao-TypeScript/src/aula060-namespaces/module.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+namespace MeuNamespace {
+  export const nomeDoNamespace = 'Luiz';
+
+  export class PessoaDoNamespace {
+    constructor(public nome: string) {}
+  }
+
+  const pessoaDoNamespace = new PessoaDoNamespace('Luiz');
+
+  export namespace OutroNamespace {
+    export const nomeDoNamespace = 'Nome no outro namespace';
+  }
+}
+
+const constDoNamespace = 'Valor da const do namespace';
+
+export default 1;
