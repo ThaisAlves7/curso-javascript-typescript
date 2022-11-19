07c14fd02e2f5e31d6cfff8a34a21d783607c4fb
Thais Alves <thacruzz007@gmail.com>
2022-11-19 00:27:34 -0300

    OCP - Open/ Closed Principle
     Entidades devem estar abertas para extensão mas fechadas para modificação

---
+++ Secao-SOLID/src/ocp/main.ts
@@ -0,0 +1,30 @@
+/*
+  Open/ Closed Principle
+    Entidades devem estar abertas para extensão mas fechadas para modificação
+*/
+
+import { FiftyPercentDiscount } from './classes/discount';
+import { Order } from './classes/order';
+import { Product } from './classes/product';
+import { ShoppingCart } from './classes/shopping-cart';
+import { Messaging } from './services/messaging';
+import { Persistency } from './services/persistency';
+
+const fiftyPercentDiscount = new FiftyPercentDiscount();
+// const tenPercentDiscount = new TenPercentDiscount();
+// const noDiscount = new NoDiscount();
+const shoppingCart = new ShoppingCart(fiftyPercentDiscount);
+const messaging = new Messaging();
+const persistency = new Persistency();
+const order = new Order(shoppingCart, messaging, persistency);
+
+shoppingCart.addItem(new Product('Camiseta', 49.91));
+shoppingCart.addItem(new Product('Caderno', 9.9123));
+shoppingCart.addItem(new Product('Lápis', 1.59));
+
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+console.log(shoppingCart.totalWithDiscount());
+console.log(order.orderStatus);
+order.checkout();
+console.log(order.orderStatus);
